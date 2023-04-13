23ab4d1caed2f3cf7fba0479f675fcc0baccacef
Ola <hollabodey.kilaso@gmail.com>
2023-04-13 22:51:24 +0100

    Made changes to the html and css file

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 console.log("Connected successfully!");
+
+const reset = document.querySelector(".reset");
+const resultAmount =document.querySelector("#resultAmnt");
+const resultTotal = document.querySelector("#resultTotal");
+const custom = document.getElementById("custom");
+const peopleInput = document.getElementById("people-input");
+const billInput = document.getElementById("bill-input");
+const tipPercent = document.querySelectorAll(".btn");
+
+tipPercent.forEach((tips) => {
+    tips.addEventListener("click", function(e) {
+        let tipvalue = e.target.innerText;
+        console.log("tip value: " + tipvalue);
+    });
+    
+});
+
+reset.addEventListener("click", function(){
+    resultAmount.innerHTML = "0.00";
+    resultTotal.innerHTML = "0.00";
+    custom.value = "";
+    peopleInput.value = "";
+    billInput.value = "";
+
+});
+
