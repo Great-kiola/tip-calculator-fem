b12844a1823c867fb32c120946b5468cb6dff3d8
Ola <hollabodey.kilaso@gmail.com>
2023-04-13 23:21:36 +0100

    Made changes to the js file

--- script.js
+++ script.js
@@ -8,3 +8,32 @@ const peopleInput = document.getElementById("people-input");
 const billInput = document.getElementById("bill-input");
 const tipPercent = document.querySelectorAll(".btn");
 
+billInput.addEventListener("input", billInputFun);
+peopleInput.addEventListener("input", peopleInputFun);
+tipPercent.forEach(function(val) {
+    val.addEventListener("click", handleClick);
+});
+
+function billInputFun () {
+    billValue = parseFloat(billInput.value);
+    console.log("billValue: " + billValue);
+}
+
+function peopleInputFun () {
+    peopleValue = parseFloat(peopleInput.value);
+    console.log("peopleValue: " + peopleValue);
+}
+
+function handleClick () {
+    
+}
+
+reset.addEventListener("click", function(){
+    resultAmount.innerHTML = "0.00";
+    resultTotal.innerHTML = "0.00";
+    custom.value = "";
+    peopleInput.value = "";
+    billInput.value = "";
+
+});
+
